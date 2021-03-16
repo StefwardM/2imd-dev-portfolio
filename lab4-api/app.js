ec493bd01555f3b1196271f32a8d316f4b65b5cc
StefwardM <stefanmladjenovic@hotmail.com>
2021-03-16 23:50:15 +0100

    Localstorage works now, deletes contents after 1 hour or re-uses contents if there is still content in it

--- lab4-api/app.js
+++ lab4-api/app.js
@@ -1,9 +1,13 @@
 class App{
     constructor(){
+        console.log(this.weather);
+        this.loadNotesFromStorage()
         this.getLocation();
-        this.getGenreForWeather();
+        this.getLaunchesForWeather();
         this.lat;
         this.lng;
+        this.weather;
+        this.launches
     }
     getLocation(){
         navigator.geolocation.getCurrentPosition(this.gotLocation.bind(this), this.errorLocation.bind(this));
@@ -15,26 +19,62 @@ class App{
         this.getWeather();
     }
 
-    getWeather(){
-        let url = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lng}&appid=0a925e098c03f24e81080451fedc7ae6&units=metric`;
-        fetch(url).then(response => {
-            return response.json();
-        }).then(data => {
-            document.querySelector("#weather").innerHTML = data.current.temp + data.current.weather[0].description;
-        }).catch(err => {
-            console.log(err);
-        })
+    loadNotesFromStorage() {
+        let lastclear = localStorage.getItem("lastclear"),
+         timeNow = (new Date().getTime());
+        if((timeNow - lastclear) > 1000 * 60 * 60){
+            localStorage.clear();
+            localStorage.setItem("lastclear", timeNow);
+            console.log("er is een uur voorbij");
+        }
+        this.weather = localStorage.getItem("storedWeather");
+        this.launches = localStorage.getItem("storedLaunches");
+        this.launches = JSON.parse(this.launches) || [];
+        this.weather = JSON.parse(this.weather) || [];
     }
 
-    getGenreForWeather(){
-        let url = `https://cors-anywhere.herokuapp.com/https://ll.thespacedevs.com/2.0.0/launch/`;
-        fetch(url).then(response => {
-            return response.json();
-        }).then(data => {
-            console.log(data);
-        }).catch(err => {
-            console.log(err);
-        })
+    getWeather(){
+        let url = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lng}&appid=0a925e098c03f24e81080451fedc7ae6&units=metric`;
+        if(this.weather == ""){
+            fetch(url).then(response => {
+                return response.json();
+            }).then(data => {
+                localStorage.setItem('storedWeather', JSON.stringify(data));
+                document.querySelector("#weather").innerHTML = data.current.temp + data.current.weather[0].description;
+                console.log("is niet naar else gegaan");
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+        else{
+            document.querySelector("#weather").innerHTML = this.weather.current.temp + this.weather.current.weather[0].description;
+            console.log("is naar else gegaan");
+        }
+
+    }
+
+    getLaunchesForWeather(){
+        let url = `https://cors-anywhere.herokuapp.com/https://ll.thespacedevs.com/2.2.0/event/upcoming/`;
+        if(this.launches == ""){
+            fetch(url).then(response => {
+                return response.json();
+            }).then(data => {
+                console.log(data);
+                localStorage.setItem('storedLaunches', JSON.stringify(data));
+                document.querySelector("#ad").style.backgroundImage = `url(${data.results[0].feature_image}`;
+                document.querySelector("#launch").innerHTML = data.results[0].date;
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+
+            else{
+
+                    document.querySelector("#ad").style.backgroundImage = `url(${this.launches.results.feature_image}`;
+                    document.querySelector("#launch").innerHTML = this.launches.results[0].date;
+
+            }
+
     }
 
     errorLocation(err){
